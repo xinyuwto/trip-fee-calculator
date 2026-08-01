7b04fb845b3034701647c6040716f48a531bc91e
xinyuwto <xinyuwto@gmail.com>
2026-08-02 00:29:01 +0800

    feat: add SettlementView and DetailView

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,14 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import InitView from '../views/InitView.vue'
 import ExpenseView from '../views/ExpenseView.vue'
+import SettlementView from '../views/SettlementView.vue'
+import DetailView from '../views/DetailView.vue'
 
 const routes = [
   { path: '/', name: 'init', component: InitView },
-  { path: '/expense', name: 'expense', component: ExpenseView }
+  { path: '/expense', name: 'expense', component: ExpenseView },
+  { path: '/settlement', name: 'settlement', component: SettlementView },
+  { path: '/detail', name: 'detail', component: DetailView }
 ]
 
 const router = createRouter({
